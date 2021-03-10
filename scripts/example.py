d0a74dc28b0947745085d3fc66960e4af1b5547d
Marchukov Ivan <marchukov.ivan@gmail.com>
2021-03-10 18:38:18 +0300

    Initial commit

---
+++ scripts/example.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Image
+
+
+def img_callback(img: Image):
+    rospy.loginfo('%d x %d    size: %zu', img.width, img.height, len(img.data))
+
+
+def example():
+    rospy.init_node('example', anonymous=True)
+    rospy.Subscriber('/drone_camera_264_main/image_raw', Image, img_callback, queue_size=5)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        example()
+    except rospy.ROSInterruptException:
+        pass
